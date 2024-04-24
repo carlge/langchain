6216b1a5fa0f3acbf58a480c702f74e3b8a5b2ff
Carl <gecanran@umd.edu>
2024-04-23 21:17:40 -0700

    add text langchain

---
+++ 01_langchain/01_textLangChain.py
@@ -0,0 +1,6 @@
+from langchain_community.llms import Ollama
+
+# using num_predict instead of max_tokens
+llm = Ollama(model="llama3", temperature=0.8, num_predict=60)
+response = llm.invoke("Tell me a joke")
+print(response)
